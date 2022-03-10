e1fe9c0e9da9f546beebebe36a9b2ceceece3638
zahin100 <akmalzahin01@gmail.com>
2022-03-10 16:32:04 +0800

    First Commit.

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,20 @@
+package samples.demo;
+
+public class Location 
+{
+	private int attribute;
+
+	public int getAttribute() {
+		return attribute;
+	}
+
+	public void setAttribute(int attribute) {
+		this.attribute = attribute;
+	}
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
